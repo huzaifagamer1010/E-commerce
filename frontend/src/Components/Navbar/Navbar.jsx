93fc096225cd83d1549e761e86e4a4c37c59564d
huzaifagamer1010 <younushuzaifa480@gmail.com>
2026-03-31 13:40:05 +0500

    css file added for navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -7,8 +7,18 @@ const Navbar = () => {
     <div className='navbar'>
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>Shopify</p>
+        <p>SHOPIFY</p>
       </div>
+    <ul className='nav-menu'>
+      <li>Shop</li>
+      <li>Men</li>
+      <li>Women</li>
+      <li>Kids</li>
+    </ul>
+    <div className="nav-login-cart">
+      <button>Login</button>
+      <img src={cart_icon} alt="" />
+    </div>
     </div>
   )
 }
