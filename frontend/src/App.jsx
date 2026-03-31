70f4832b96a6cf76fa6ac54b222f6bce3656bb84
huzaifagamer1010 <younushuzaifa480@gmail.com>
2026-03-31 18:19:46 +0500

    added pages in Pages/ directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Shop from './Pages/Shop'
 
 function App() {
   
@@ -8,7 +10,15 @@ function App() {
   return (
     <>
       <div>
+        <BrowserRouter>
         <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+
+        </Routes>
+
+
+        </BrowserRouter> 
       </div>
     </>
   )
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Navbar.css'
 import logo from '../Assets/Frontend_Assets/logo.png'
 import cart_icon from '../Assets/Frontend_Assets/cart_icon.png'
+
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop")
+
+
+
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -10,14 +16,15 @@ const Navbar = () => {
         <p>SHOPIFY</p>
       </div>
     <ul className='nav-menu'>
-      <li>Shop<hr/></li>
-      <li>Men</li>
-      <li>Women</li>
-      <li>Kids</li>
+      <li onClick={()=>{setMenu('shop')}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu('menu')}}>Men{menu==="menu"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu('womens')}}>Women{menu==="womens"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu('kids')}}>Kids{menu==="kids"?<hr/>:<></>}</li>
     </ul>
     <div className="nav-login-cart">
       <button>Login</button>
       <img src={cart_icon} alt="" />
+      <div className="nav-cart-count">0</div>
     </div>
     </div>
   )
