dbd8247d57ba85d558db6ed8bb63d66ac2f9f9da
huzaifagamer1010 <younushuzaifa480@gmail.com>
2026-04-02 14:49:20 +0500

    css for files updated

---
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NewCollections.css'
+
+
+const NewCollections = () => {
+  return (
+    <div className='newcollections'>
+      
+    </div>
+  )
+}
+
+export default NewCollections
