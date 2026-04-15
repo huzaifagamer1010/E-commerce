dd9054ba8a65d5e2caae30fe55d3659f8e2970af
huzaifagamer1010 <younushuzaifa480@gmail.com>
2026-04-15 20:36:23 +0500

    ecommerce home work update

---
+++ frontend/src/Components/Breadcrumbs/BreadCrumbs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Breadcrumbs.css'
+import arrow_icon from '../Assets/Frontend_Assets/breadcrum_arrow.png'
+
+const BreadCrumbs = (props) => {
+    const {product} = props;
+  return (
+    <div className='breadcrumbs'>
+        HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+    </div>
+  )
+}
+
+export default BreadCrumbs
